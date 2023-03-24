8f1ed3745f1e20a6be01758ba7de6559c2cedb77
alemonacoo <ale.monaco@outlook.com>
2023-03-24 18:34:29 +0100

    adding api logic

--- src/logic/api.js
+++ src/logic/api.js
@@ -1,5 +1,19 @@
 import axios from "axios";
 
-const searchImages = () => {
-  axios.get(process.URL);
+const searchImages = async (query) => {
+  try {
+    const response = await axios.get(process.env.REACT_APP_API_URL, {
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`,
+      },
+      params: {
+        query: query,
+      },
+    });
+    return response.data.results;
+  } catch (e) {
+    return e;
+  }
 };
+
+export default searchImages;
