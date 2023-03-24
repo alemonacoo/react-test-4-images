50f1e90326446445919da8afea0cf6e32c2ded00
alemonacoo <ale.monaco@outlook.com>
2023-03-24 16:19:53 +0100

    creating api file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchBar from "./components/SearchBar";
 import "./style/App.css";
 
 function App() {
+  console.log(process.env.REACT_APP_API_URL);
   return (
     <div className="App">
       <Header title="React Test 4 - Images" />
