7c58a0f5c62d112a1d51a6a77367180e3caa6838
alemonacoo <ale.monaco@outlook.com>
2023-03-24 18:33:46 +0100

    completing searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,30 @@
 import "../style/SearchBar.css";
+import { useState } from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(term);
+  };
+
+  const handleInputChange = (event) => {
+    setTerm(event.target.value);
+  };
+
   return (
-    <div className="SearchBarContainer">
-      <input type="text" placeholder="search" />
-      <button>Search</button>
-    </div>
+    <form onSubmit={handleFormSubmit}>
+      <div className="SearchBarContainer">
+        <input
+          value={term}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="search"
+        />
+        <button>Search</button>
+      </div>
+    </form>
   );
 };
 
